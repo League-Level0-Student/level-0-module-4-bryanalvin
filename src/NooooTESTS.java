309f5e659f34bedd6c3c72330a23986b889186c2
ilabstaff <ilabstaff@172.16.116.73>
2019-03-19 18:03:53 -0700

    Do this test

---
+++ src/NooooTESTS.java
@@ -0,0 +1,33 @@
+import javax.swing.JOptionPane;
+
+public class NooooTESTS {
+public static void main(String[] args) {
+	String boo=JOptionPane.showInputDialog("ok students turn in your tests the lowest grade is yandere and the highest grade is DereDere ");
+	int Look=Integer.parseInt(boo);
+	if(Look==0) {
+		JOptionPane.showMessageDialog(null,"You got an F that means your a Yandere" );
+	}
+	if(Look==1)
+	JOptionPane.showMessageDialog(null,"You got a D- you are the Coodere" );
+	
+	if(Look==2)
+	JOptionPane.showMessageDialog(null,"You got a D you are an Undere " );
+	
+	if(Look==3)
+	JOptionPane.showMessageDialog(null,"You got a D+ Dandere for you " );
+	if(Look==4)
+	JOptionPane.showMessageDialog(null,"You got a C the unemotionel type Kuudere" );
+	
+	if(Look==5)
+	JOptionPane.showMessageDialog(null,"You got a C+ Tsundere you are" );
+	
+	if(Look==6)
+	JOptionPane.showMessageDialog(null,"You got a B- uhhh Himedere" );
+	if(Look==7)
+	JOptionPane.showMessageDialog(null,"You got a B the goddes of deres kamidere " );
+	if(Look==8)
+	JOptionPane.showMessageDialog(null,"You got a B+ DereDere " );
+	
+	
+}
+}
