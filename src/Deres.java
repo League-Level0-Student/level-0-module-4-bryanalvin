f349a0f6614ed4ddd53c0a741e35b44efbb586df
ilabstaff <ilabstaff@172.16.116.67>
2019-03-25 17:52:57 -0700

    Its not finished yet if i had more time than it will be finished maybe enjoy

---
+++ src/Deres.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+public class Deres {
+public static void main(String[] args) {
+JOptionPane.showMessageDialog(null,"There was once a boy who was all about the anime life he watched it day in and day out but one day he had a dream about anime" );
+JOptionPane.showMessageDialog(null,"There was pots and pans,ramen and Girls and the SEVEN DEADLY SINS TOO!He see's Two paths one to Dere land and one to choas world.");
+String blade=JOptionPane.showInputDialog("Which path should he take the Chaos path or Dere land ");
+JOptionPane.showMessageDialog(null,"Onwards he goes to Dere land The place for Happy,Annoying And Creepy anime girls diffrent types as well as secretive ones too");
+JOptionPane.showMessageDialog(null,"Along the way he meets June,Kyoko,Haru and Chiaki they alll were strange in their own ways but they were all right i guess but the hardships he will have to face will be tough but he will get through it.");
+}
+}
