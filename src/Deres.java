b8f99442311c54fc58fb7b2727eae9d19edfb2e3
ilabstaff <ilabstaff@172.16.116.82>
2019-04-08 17:53:07 -0700

    read read read read

--- src/Deres.java
+++ src/Deres.java
@@ -7,5 +7,9 @@ JOptionPane.showMessageDialog(null,"There was pots and pans,ramen and Girls and 
 String blade=JOptionPane.showInputDialog("Which path should he take the Chaos path or Dere land ");
 JOptionPane.showMessageDialog(null,"Onwards he goes to Dere land The place for Happy,Annoying And Creepy anime girls diffrent types as well as secretive ones too");
 JOptionPane.showMessageDialog(null,"Along the way he meets June,Kyoko,Haru and Chiaki they alll were strange in their own ways but they were all right i guess but the hardships he will have to face will be tough but he will get through it.");
+JOptionPane.showMessageDialog(null,"They all run into a Big monster snake thing they have no weapons but then a giant sword appeared in the boy's hand and with great aim a slice to the beast's neck and it died. ");
+JOptionPane.showMessageDialog(null,"The boy held te sword in his hands for the rest of his jury and the group runs into a harem a big one too");
+JOptionPane.showMessageDialog(null,"The group was no match for the wave of girls in their path so the boy did something no one would ever do. ");
+String dead=JOptionPane.showInputDialog("");
 }
 }
