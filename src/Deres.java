b3b4090595c66e71f7f078466c9db6c76a4f008d
ilabstaff <ilabstaff@172.16.116.81>
2019-04-09 17:54:33 -0700

    The best fight in anime will come out soon

--- src/Deres.java
+++ src/Deres.java
@@ -5,11 +5,24 @@ public static void main(String[] args) {
 JOptionPane.showMessageDialog(null,"There was once a boy who was all about the anime life he watched it day in and day out but one day he had a dream about anime" );
 JOptionPane.showMessageDialog(null,"There was pots and pans,ramen and Girls and the SEVEN DEADLY SINS TOO!He see's Two paths one to Dere land and one to choas world.");
 String blade=JOptionPane.showInputDialog("Which path should he take the Chaos path or Dere land ");
+if(blade.equals("Chaos Path")) {
+	JOptionPane.showMessageDialog(null, "The boy goes to Choas path along the way he runs into a monster that swallows him up and he dies in the dream and wakes up in real life.");
+}
+	if(blade.equals("Dere land"))
 JOptionPane.showMessageDialog(null,"Onwards he goes to Dere land The place for Happy,Annoying And Creepy anime girls diffrent types as well as secretive ones too");
 JOptionPane.showMessageDialog(null,"Along the way he meets June,Kyoko,Haru and Chiaki they alll were strange in their own ways but they were all right i guess but the hardships he will have to face will be tough but he will get through it.");
 JOptionPane.showMessageDialog(null,"They all run into a Big monster snake thing they have no weapons but then a giant sword appeared in the boy's hand and with great aim a slice to the beast's neck and it died. ");
-JOptionPane.showMessageDialog(null,"The boy held te sword in his hands for the rest of his jury and the group runs into a harem a big one too");
+JOptionPane.showMessageDialog(null,"The boy held the sword in his hands for the rest of his journey and the group runs into a harem a big one too");
 JOptionPane.showMessageDialog(null,"The group was no match for the wave of girls in their path so the boy did something no one would ever do. ");
-String dead=JOptionPane.showInputDialog("");
+String dead=JOptionPane.showInputDialog("What should he do kiss the girl or slice them to pieces?");
+if(dead.equals("Kiss a girl and start a fight")) {
+	JOptionPane.showMessageDialog(null, "He kisses one of the girls with passion and the girls start fighting for the boy and him and his companions walked away it seems Chiaki was very triggered from that");
+}
+	
+if(dead.equals("Slice them to peices and your companions run away from you")) {
+	JOptionPane.showMessageDialog(null, "The boy held his sword and sliced all the girls into little peices in one go all of his companions saw him and left the boy alone to find new companions");	
+	}
+JOptionPane.showMessageDialog(null, "The group falls down a dark pit to choas path and they have to get back to dere land good thing the boy is prepared");
+
 }
 }
