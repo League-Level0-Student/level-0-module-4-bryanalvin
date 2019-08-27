845063f1ddbdc594a88fc20b46802ee750589fec
ilabstaff <ilabstaff@172.16.116.68>
2019-08-26 17:53:45 -0700

    See you next week

--- src/charAt_scanners_optionDialogs/PiAloud.java
+++ src/charAt_scanners_optionDialogs/PiAloud.java
@@ -9,22 +9,38 @@ import java.util.Scanner;
 public class PiAloud {
 public static void main(String[] args) {
 	
-}
+
 	// 1. Make a main method and make sure your program can run
 
-	// 2. Make a String variable to hold the value of Pi. You could use http://www.piday.org/million/ for the value.
+	
 
-	// 3. Print out some digits of Pi. The first value is "pi.charAt(0)", the second is "pi.charAt(1)"
+	// 2. Make a String variable to hold the value of Pi. You could use http://www.piday.org/million/ for the value
 
+	String Pie="3.14159265358979323846264338327950288419716939937510582097494459230781640628620899862803482534211706798214808651328230664709384460955058223172535";
+	// 3. Print out some digits of Pi. The first value is "pi.charAt(0)", the second is "pi.charAt(1)
+	System.out.println(Pie.charAt(0)+"  "+Pie.charAt(1));
+
+		
+	
+		
+		
+		
+	
+	
+		
 	// 4. Print ALL the digits of of Pi (hint: use a loop)
-
+	for (int i = 0; i < Pie.length(); i++) {
+		System.out.println(Pie.charAt(i));
+		speak(Pie.charAt(i));
+	}
+	
 	// 5. Use the speak() method to speak all the digits of Pi.
 
 	// [ADVANCED]
 	// *6. Get a character from the user using the getInputFromUser() method
 	// *7. Compare the users' char to the next digit of Pi
 	// *8. If they are correct, print out "correct". If they are not, print "incorrect" to System.err.println
-
+}
 	static void speak(char characterToSpeak) {
 		try {
 			Runtime.getRuntime().exec("say " + characterToSpeak).waitFor();
--- src/charAt_scanners_optionDialogs/StringMethods1.java
+++ src/charAt_scanners_optionDialogs/StringMethods1.java
@@ -2,22 +2,24 @@ package charAt_scanners_optionDialogs;
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
 
-
+import javax.swing.JOptionPane;
 
 public class StringMethods1 {
 	public static void main(String[] args) {
 		
 		// 1. Create a String variable and initialize it to whatever you want
-
+String Doo="Play monster hunter world it's the greatest i'm a switch axe main charge blade is to complicated.";
 		// 2. Print the 3rd char of your String to the console.
 		//    HINT: .charAt
-		
+		System.out.println(Doo.charAt(2));
 		// 3. Print the length of your String to the console.
 		//    HINT: .length()
-
+		System.out.println(Doo.length());
 		// 4. Using a for loop, print one char at time to the console.
 		//    HINT: use .length() to determine how many loops
-
+for (int i = 0; i < Doo.length(); i++) {
+	System.out.println(Doo.charAt(0));
+}
 		
 		// 5. Pick a char inside your String, and use a loop to determine
 		//    what position/index in the String the char is located.
